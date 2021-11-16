b0b444c120e357cd3313e35df538b1c794253c83
João Lucas <introducaoteste@gmail.com>
2021-11-16 05:25:35 -0400

    Atualizando página de busca de personagens

--- src/Pages/Personagens/index.js
+++ src/Pages/Personagens/index.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState} from 'react'
+import React, { useState} from 'react'
 import { HiOutlineSearch } from 'react-icons/hi'
 import { HeaderResponsive } from '../../Components/HeaderResponsive'
 import { api } from '../../Api'
-import './styles.css'
+import './style.css'
+import { Card } from '../../Components/Card'
 
 
 export function Personagens() {
     const [data, setData] = useState(null)
     const [name, setName] = useState('')
 
-    useEffect(() => {
-        async function loadAll() {
-            let res = await api.getCharacterByName()
-            setData(res.data)
-        }
-        loadAll()
-    }, [data])
+    async function load() {
+        let res = await api.getCharacterByName(name)
+        setData(res)
+    }
 
-    function getInputValue(e) {
-        console.log('Nome: ' + name)
+    function searchCharacter(e) {
+        e.preventDefault()
+        setData(null)
+        if (name) {
+            load()
+        }
     }
 
     return (
         <div>
             <HeaderResponsive />
             <div className="search-container">
-                <form onSubmit={getInputValue}>
+                <form onSubmit={searchCharacter}>
                     <div>
                         <input type="text" placeholder="nome do personagem" onChange={(e) => {
                             setName(e.target.value)
@@ -36,8 +38,12 @@ export function Personagens() {
                     </div>
                 </form>
             </div>
+            <div className="results-container">
+                {data && ((data.error) ? <p>Busca não encontrada</p> : 
+                (data.results.map((item, key) => {
+                    return <Card id={item.id} img={item.image} name={item.name} />
+                })))}
+            </div>
         </div>
     )
 }
-
-// continuar 02:10:30
